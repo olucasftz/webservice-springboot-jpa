f880e9bc51b321784edbf99e32e02caf7bfedfe6
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-24 10:04:20 -0300

    Project created

---
+++ src/main/java/com/devsuperior/coursewebservice/CourseWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.coursewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWebserviceApplication.class, args);
+	}
+
+}
