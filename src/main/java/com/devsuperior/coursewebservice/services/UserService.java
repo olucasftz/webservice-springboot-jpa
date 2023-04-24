6699bbfcbc4e0467b26e0cc2de8f568777f7e182
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-24 12:54:22 -0300

    created service layer, component registration and two news requisitions

---
+++ src/main/java/com/devsuperior/coursewebservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.coursewebservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.coursewebservice.entities.User;
+import com.devsuperior.coursewebservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired 
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
