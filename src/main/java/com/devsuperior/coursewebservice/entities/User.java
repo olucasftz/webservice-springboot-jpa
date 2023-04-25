faba8b61298098fab018177993bc0534ff4a45ce
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-25 11:51:36 -0300

    feat: I implemented the first part of the order entity and the date with Instant

--- src/main/java/com/devsuperior/coursewebservice/entities/User.java
+++ src/main/java/com/devsuperior/coursewebservice/entities/User.java
@@ -1,18 +1,20 @@
 package com.devsuperior.coursewebservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable{
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -22,6 +24,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -74,6 +79,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
